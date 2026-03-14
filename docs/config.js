7091a713b635536b1e11264f6b7eeda36d4960dd
Muhammad Allak <blawa3i@gmail.com>
2026-03-14 02:31:14 +0000

    Configure deployed frontend endpoints

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'http://localhost:4000',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  API_BASE_URL: 'https://cadeau-hbmt.onrender.com',
+  SUPABASE_URL: 'https://llrpiecwnuvgrdjrfdjm.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxscnBpZWN3bnV2Z3JkanJmZGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNjMzMDcsImV4cCI6MjA4NzYzOTMwN30.vvyP8w-1I-q6JAEsuSisDVApcgpt_R5Ecf0FjrlAwAk',
 };
